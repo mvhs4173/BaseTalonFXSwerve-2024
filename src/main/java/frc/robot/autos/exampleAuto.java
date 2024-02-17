e9c3c7ba7f967cbbbb8b2124fbb7f2c2692da363
FRC 4173: IMVERT Robotics <mvhs4173@gmail.com>
2024-02-16 17:40:38 -0800

    used units to change meters to feet

--- src/main/java/frc/robot/autos/exampleAuto.java
+++ src/main/java/frc/robot/autos/exampleAuto.java
@@ -13,6 +13,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
@@ -29,11 +30,13 @@ public class exampleAuto extends SequentialCommandGroup {
         Trajectory exampleTrajectory =
             TrajectoryGenerator.generateTrajectory(
                 // Start at the origin facing the +X direction
-                new Pose2d(0, 0, new Rotation2d(0)),
+                new Pose2d(Units.feetToMeters(0), Units.feetToMeters(0), new Rotation2d(Units.degreesToRadians(0))),
                 // Pass through these two interior waypoints, making an 's' curve path
-                List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+                List.of(
+                    new Translation2d(Units.feetToMeters(13.66), Units.feetToMeters(0)),
+                    new Translation2d(Units.feetToMeters(13.66), Units.feetToMeters(3))),
                 // End 3 meters straight ahead of where we started, facing forward
-                new Pose2d(3, 0, new Rotation2d(0)),
+                new Pose2d(Units.feetToMeters(15.66), Units.feetToMeters(3), new Rotation2d(Units.degreesToRadians(0))),
                 config);
 
         var thetaController =
